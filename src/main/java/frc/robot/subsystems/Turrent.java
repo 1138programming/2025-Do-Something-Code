7353fd0209382d248b3c14c818c9c3686766ebe6
Goomisica <60203441+Goomisica@users.noreply.github.com>
2025-11-20 17:07:01 -0800

    Turrent

---
+++ src/main/java/frc/robot/subsystems/Turrent.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import static frc.robot.Constants.OperatorConstants.*;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public class Turrent extends SubsystemBase {
+    private SparkMax rotationMotor;
+
+    public Turrent() {
+        rotationMotor = new SparkMax(kMotorId,MotorType.kBrushless);
+    }
+
+    public void rotate(double s) {
+        rotationMotor.set(s);
+    }
+    
+    @Override
+    public void periodic() {
+
+    }
+}
